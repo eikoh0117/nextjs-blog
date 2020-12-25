fb880c92e60156131b54025b6c8dd5bf9fe30c50
Hidemitsu Aoki <olympos0117@gmail.com>
2020-12-25 12:50:10 +0900

    Add Head

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,8 +1,9 @@
+import Head from 'next/head'
 import Layout from '../../components/layout'
 import { getAllPostIds, getPostData } from '../../lib/posts'
 
 export async function getStaticProps({ params }) {
-  const postData = getPostData(params.id)
+  const postData = await getPostData(params.id)
   return {
     props: {
       postData
@@ -17,14 +18,20 @@ export async function getStaticPaths() {
     fallback: false
   }
 }
+
 export default function Post({ postData }) {
   return (
     <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
       {postData.title}
       <br />
       {postData.id}
       <br />
       {postData.date}
+      <br />
+      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
     </Layout>
   )
 }
