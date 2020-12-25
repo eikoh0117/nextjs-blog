e9db0d6c6ec146a46c42d04df2317ce6d639d8a4
Hidemitsu Aoki <olympos0117@gmail.com>
2020-12-25 15:12:26 +0900

    Revert "Use API routes"
    This reverts commit f2a144c532bb48fdf6298e0aa85586516298720e.

--- pages/api/hello.js
+++
@@ -1,4 +0,0 @@
-export default function handler(req, res) {
-  console.log(res.status(200))
-  res.status(200).json({ text: 'Hello' })
-}
--- pages/api/post/[pid].js
+++
@@ -1,7 +0,0 @@
-export default (req, res) => {
-  const {
-    query: { pid },
-  } = req
-  console.log("req", req)
-  res.end(`Post: ${pid}`)
-}
